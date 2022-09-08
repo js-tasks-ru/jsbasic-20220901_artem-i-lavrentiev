85cd74ca8f2a8aefc0489f5b00895e59c2ea564c
ailavrentiev <artem.i.lavrentiev@gmail.com>
2022-09-08 22:22:54 +0300

    add fixes

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,13 +10,7 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  let result = false;
-
-  if (name !==null && !name.includes(" ") && name.length > 3) {
-    result = true;
-  }
-
-  return result;
+  return name !==null && !name.includes(" ") && name.length > 3;
 }
 
 function sayHello() {
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,10 +1,5 @@
 function checkSpam(str) {
-  let result = false;
   str = str.toUpperCase();
 
-  if (str.includes('1XBET') || str.includes('XXX')) {
-    result = true;
-  }
-
-  return result;
+  return str.includes('1XBET') || str.includes('XXX');
 }
