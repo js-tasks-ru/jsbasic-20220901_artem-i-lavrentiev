3fa57213bab2d7e4c2fe337c78aed0aafa5ddbc8
ailavrentiev <artem.i.lavrentiev@gmail.com>
2022-09-24 21:41:21 +0300

    add MakeFriendList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement('ul');
+  friends.forEach(element => {
+    let li = document.createElement('li');
+    li.textContent = element.firstName + ' ' + element.lastName;
+    ul.append(li);
+  });
+  return ul;
 }
