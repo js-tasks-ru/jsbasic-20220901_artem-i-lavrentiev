cfe2992ff39361c0d16b1dee9aa47f59571f32a9
LAVRENTYEV Artem <Artem Lavrentiev>
2022-10-06 13:53:01 +0300

    add highlight

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,25 +1,3 @@
 function highlight(table) {
-  for (let i = 0; i < table.rows.length; i++) {
-    let tableRow = table.rows[i];
-    for (let j = 0; j < tableRow.cells.length; j++) {
-      let tableCell = tableRow.cells[j];
-      if (j == 1) {
-        if (tableCell.innerHTML < 18) {
-          tableRow.style.textDecoration = 'line-through';
-        }
-      }
-      if (j == 2) {
-        tableRow.classList.add(tableCell.innerHTML == 'm' ? 'male' : 'female');
-      }
-      if (j == 3) {
-        if (tableCell.hasAttribute('data-available')) {
-          let attrAvailable = tableCell.dataset.available;
-          tableRow.classList.add(attrAvailable == 'true' ? 'available' : 'unavailable');
-        }
-        else {
-          tableRow.hidden = true;
-        }
-      }
-    }
-  };
+  // ваш код...
 }
