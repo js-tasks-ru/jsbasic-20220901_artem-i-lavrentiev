bcda92bfd1b347f393af9ba597315fbe80fff120
Artem Lavrentiev <artem.i.lavrentiev@gmail.com>
2022-10-08 21:09:06 +0300

    5-module fix

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,5 +1,6 @@
 function initCarousel() {
   const slide = document.querySelector('.carousel__inner');
+  const slideCount = slide.children.length;
   const arrowRight = document.querySelector('.carousel__arrow_right');
   const arrowLeft = document.querySelector('.carousel__arrow_left');
   let slideOffset = slide.offsetWidth;
@@ -9,7 +10,7 @@ function initCarousel() {
     translation += slideOffset;
     slide.style.transform = `translateX(-${translation}px)`;
     translation == 0 ? arrowLeft.style.display = 'none' : arrowLeft.style.display = '';
-    translation == slideOffset*3 ? arrowRight.style.display = 'none' : arrowRight.style.display = '';
+    translation == slideOffset*(slideCount-1) ? arrowRight.style.display = 'none' : arrowRight.style.display = '';
   });
   arrowLeft.addEventListener('click', () => {
     console.log(translation);
@@ -17,6 +18,6 @@ function initCarousel() {
     console.log(translation);
     slide.style.transform = `translateX(-${translation}px)`;
     translation == 0 ? arrowLeft.style.display = 'none' : arrowLeft.style.display = '';
-    translation == slideOffset*3 ? arrowRight.style.display = 'none' : arrowRight.style.display = '';
+    translation == slideOffset*(slideCount-1) ? arrowRight.style.display = 'none' : arrowRight.style.display = '';
   });
 }
