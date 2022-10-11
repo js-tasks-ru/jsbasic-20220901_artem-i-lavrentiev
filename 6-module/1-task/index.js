f1f5b0ee80d6d50d1c87e96b17904d857720e3cf
Artem Lavrentiev <artem.i.lavrentiev@gmail.com>
2022-10-11 21:26:04 +0300

    UserTable

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,50 @@
  *
  */
 export default class UserTable {
+  #rows = null;
+
   constructor(rows) {
+    this.#rows = rows;
+    this.elem = this.#render();
+  }
+
+  #render() {
+    const html = this.#template();
+
+    const elem = document.createElement('table');
+    elem.innerHTML = html;
+
+    const buttons = elem.querySelectorAll('button');
+    for (const button of buttons) {
+      button.addEventListener('click', this.#onButtonClick);
+    }
+
+    return elem;
+  }
+
+  #onButtonClick() {
+    this.parentElement.parentElement.remove();
+  }
+
+  #template() {
+    return `
+    <thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+      </tr>
+    </thead>
+      ${this.#rows.map(item => 
+        `<tr>
+          <td>${item.name}</td>
+          <td>${item.age}</td>
+          <td>${item.salary}</td>
+          <td>${item.city}</td>
+          <td><button>X</buton></td>
+        </tr>`)
+      .join('\n')}
+    `
   }
 }
