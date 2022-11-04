49526098754dc880de7d943718da790ab5dafa26
Artem Lavrentiev <artem.i.lavrentiev@gmail.com>
2022-11-04 20:50:31 +0300

    add ProductGrid

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -13,7 +13,19 @@ export default class ProductGrid {
     let filterProducts = this.products.filter(this.#generateFilter, this.filters);
     this.#getProductGridInner(filterProducts);
   }
-}
+
+  #generateFilter(product) {
+    if (this.noNuts && product.nuts) {return false;}
+    if (this.vegeterianOnly && this.vegeterianOnly !== product.vegeterian) {return false;}
+    if (this.maxSpiciness && this.maxSpiciness < product.spiciness) {return false;}
+    if (this.category && product.category !== this.category) {return false;}
+    return true;
+  }
+
+  #render() {
+    this.elem = createElement(this.#template());
+    this.#getProductGridInner(this.products);
+  }
 
   #getProductGridInner(products) {
     const gridInner = this.elem.querySelector('.products-grid__inner');
